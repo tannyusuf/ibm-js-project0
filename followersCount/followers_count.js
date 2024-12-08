70cc16bda9af75b0d30a7d7795aec46cbd95487a
Yusuf Tan <tann.yusuf@hotmail.com>
2024-12-08 08:59:22 -0500

    initial commit

---
+++ followersCount/followers_count.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById(`countDisplay`).innerHTML=count;
+}
+
+function checkCountValue(){
+    if(count == 10){
+        alert("Your Instagram post gained 10 follewers! Congratulations!");
+
+    }
+
+    else if(count == 20){
+        alert("Your Instagram post gained 20 follewers! Keep it up!");
+    }
+}
